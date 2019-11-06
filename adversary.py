3f4be5aafd14b38ea6d6710a4d4861468fea9b2e
gray <shinjoohyung25@gmail.com>
2019-11-06 00:56:17 -0500

    fix adversary

--- adversary.py
+++ adversary.py
@@ -1,17 +1,22 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from consts import global_consts as gc
 
 
 class Adv(nn.Module):
-    def __init__(self, d, h, output_dim, dropout):
+    def __init__(self):
         super(Adv, self).__init__()
-        self.h = h
+        h = gc.config['adv_h_dim']
+        self.gru = nn.GRU(input_size=gc.dim_l, hidden_size=h)
         self.fc1 = nn.Linear(h, h)
-        self.fc2 = nn.Linear(h, output_dim)
-        self.dropout = nn.Dropout(dropout)
+        out_dim = 1
+        if gc.dataset == 'mosei_emo':
+            out_dim = 6
+        self.fc2 = nn.Linear(h, out_dim)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
+        _, gru_last_h = self.gru(x.transpose(0, 1))
+        x = F.relu(self.fc1(gru_last_h.squeeze())).squeeze()
         x = F.relu(self.fc2(x))
         return x
 
